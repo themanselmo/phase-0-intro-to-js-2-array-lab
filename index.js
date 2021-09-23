cfa4d8893aa019d8b7b51ea41ed122b4386b4bef
max anselmo <itmanselmo@gmail.com>
2021-09-22 21:58:21 -0400

    completed functions according to the test suite, assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name) => {
+    cats.push(name);
+}
+
+const destructivelyPrependCat = (name) => {
+    cats.unshift(name);
+}
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop();
+}
+
+const destructivelyRemoveFirstCat = () => {
+    cats.shift();
+}
+
+const appendCat = (name) => {
+   
+    return [...cats, name];
+}
+
+const prependCat = (name) => {
+    return [name, ...cats];
+}
+
+const removeLastCat = () => {
+    return cats.slice(0,cats.length-1);
+}
+
+const removeFirstCat = () => {
+    return cats.slice(1);
+}
